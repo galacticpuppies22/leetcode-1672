7e43f6910c07798d2ce773f80b4688d2a69325f8
B H <dubmh324@gmail.com>
2024-11-23 21:33:38 -0500

    setup problem solving infrastructure

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,24 @@
-cmake -S . -B build
+#!/bin/bash
+
+# Check if the first argument is "DEBUG"
+if [ "$1" == "DEBUG" ]; then
+    BUILD_TYPE=Debug
+    echo "Debug mode enabled"
+else
+    BUILD_TYPE=Release
+fi
+
+# Generate build files with the appropriate build type
+cmake -S . -B build -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+
+# Build the project
 cmake --build build
-echo "------------------------Running Tests------------------------"
-./build/myexample
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "------------------------Running Tests------------------------"
+    ./build/myexample
+else
+    echo "Build failed. Tests will not run."
+    exit 1
+fi
